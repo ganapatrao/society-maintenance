a850e31966c368087678716080887882ca2ec709
Supresh Colaco <supresh_colaco@persistent.com>
2020-08-10 16:01:18 +0530

    adding component to route

--- src/app/router/router.module.ts
+++ src/app/router/router.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { WaterReadingComponent } from '../water/water-reading/water-reading.component';
-import { AddWaterReadingComponent } from '../water/add-water-reading/add-water-reading.component';
+import { GenerateWaterbillComponent } from '../water/generate-waterbill/generate-waterbill.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	//	{ path: '', redirectTo: '/waterbill', pathMatch: 'full' },
-	//{ path: 'waterReading', component: WaterReadingComponent },
-	{ path: 'addwaterBill', component: WaterReadingComponent }
+	{ path: 'waterReading', component: WaterReadingComponent },
+	{ path: 'addwaterBill', component: GenerateWaterbillComponent }
 ];
 
 @NgModule({
-	imports: [ RouterModule.forRoot(routes) ],
-	exports: [ RouterModule ]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class RoutingModule {}
+export class RoutingModule { }
